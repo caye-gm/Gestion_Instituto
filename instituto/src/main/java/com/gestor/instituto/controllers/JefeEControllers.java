c1625daabc399b6ae8e7c89d28e8493e64a57fc9
caye <cayetanog2000@gmail.com>
2020-10-19 00:11:57 +0200

    varias plantillas de je desarrolladas

--- instituto/src/main/java/com/gestor/instituto/controllers/JefeEControllers.java
+++ instituto/src/main/java/com/gestor/instituto/controllers/JefeEControllers.java
@@ -7,14 +7,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/jefe_de_estudio")
 public class JefeEControllers {
 
-
-
-
-
-
         @GetMapping("/")
         public String index() {
-
             return "/jefe_de_estudio/index";
         }
+
+        @GetMapping("/alumnos")
+        public String alumnos() {
+        return "/jefe_de_estudio/alumnos";
+        }
+
+        @GetMapping("/alumnoRegistro")
+        public String alumnoRegistro() {
+                return "/jefe_de_estudio/alumnoRegistro";
+        }
+        @GetMapping("/docentes")
+        public String docentes() {
+                return "/jefe_de_estudio/docentes";
+        }
+
+        @GetMapping("/docenteRegistro")
+        public String docenteRegistro() {
+                return "/jefe_de_estudio/docenteRegistro";
+        }
+        @GetMapping("/curso")
+        public String curso() {
+                return "/jefe_de_estudio/curso";
+        }
+
 }
